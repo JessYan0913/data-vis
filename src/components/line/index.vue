d716c54a3915650f6b179217f4d431d0cb26439a
严恒 <yanheng@tinkers.com.cn>
2021-01-07 14:54:36 +0800

    哈哈哈成功了

--- src/components/line/index.vue
+++ src/components/line/index.vue
@@ -8,16 +8,18 @@ import BarChart from '../../basic/bar';
 let data = [
   [1, 20, 50, 4],
   [2, 1, 23, 11],
-  [3, 3, 30, 13]
+  [3, 3, 30, 13],
+  [33, 23, 10, 13]
 ];
 
 export default {
   mounted() {
     let chart = new BarChart('#div');
     chart
-      .width(500)
-      .height(500)
+      .width(400)
+      .height(400)
       .data(data)
+      .y(d => d)
       .colors(['#134b71', '#ccff00', '#c08996'])
       .draw();
   }
